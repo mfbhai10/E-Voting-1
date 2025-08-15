1bd6253019eaa8e47b8fae327fd29278e50a4530
Mutasim fuad Rafi <mutasimfuadrafi10@gmail.com>
2025-08-15 21:52:24 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+function redirectToLogin() {
+  window.location.href = "login_SignUp.html";
+}
+function redirectToStart() {
+  window.location.href = "index.html";
+}
+function redirectToHome() {
+  window.location.href = "home.html";
+}
